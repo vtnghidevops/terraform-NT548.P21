77c23052da30b101f1f71f9acee2b16caac1b0bb
vtnghidevops <vtnghidevops@gmail.com>
2025-06-08 20:00:18 +0700

    feat: add S3 bucket for Terraform state storage

--- terraform/modules/backend/outputs.tf
+++ terraform/modules/backend/outputs.tf
@@ -8,12 +8,22 @@ output "dynamodb_table_arn" {
   value       = aws_dynamodb_table.terraform_locks.arn
 }
 
-output "kms_key_id" {
+output "kms_dynamodb_key_id" {
   description = "The ID of the KMS key used for DynamoDB encryption"
   value       = aws_kms_key.dynamodb_key.id
 }
 
-output "kms_key_arn" {
+output "kms_dynamodb_key_arn" {
   description = "The ARN of the KMS key used for DynamoDB encryption"
   value       = aws_kms_key.dynamodb_key.arn
+}
+
+output "s3_bucket_id" {
+  description = "The ID of the S3 bucket used for Terraform state storage"
+  value       = aws_s3_bucket.terraform_state.id
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket used for Terraform state storage"
+  value       = aws_s3_bucket.terraform_state.arn
 } 
