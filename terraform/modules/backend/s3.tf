ee1eda0e2f8c8a88f080f86b801074a82166f773
vtnghidevops <vtnghidevops@gmail.com>
2025-06-08 19:52:00 +0700

    fix: rm s3 config in child module

--- terraform/modules/backend/s3.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bucket-lab2-group8"
-    key    = "terraform/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform-locks-lab2-group8"
-  }
-}
