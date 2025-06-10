c8ce192c9b925a76320805a409b4d38e0b680b9c
vtnghidevops <vtnghidevops@gmail.com>
2025-06-10 17:35:33 +0700

    feat: add bootstrap module

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-lab2-group8"
-    key            = "terraform/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks-lab2-group8"  # Sẽ được thay thế bằng dynamodb_table_name trong tương lai
+    bucket = "terraform-state-bucket-lab2-group8"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locks-lab2-group8"
   }
 }
