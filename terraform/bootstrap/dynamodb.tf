5e34abb5f3fbc0c4b1f8ab1f854c60dbaa8caf97
vtnghidevops <vtnghidevops@gmail.com>
2025-06-13 17:55:49 +0700

    fix: correct syntax

--- terraform/bootstrap/dynamodb.tf
+++ terraform/bootstrap/dynamodb.tf
@@ -3,15 +3,6 @@ resource "aws_dynamodb_table" "terraform_locks" {
   billing_mode   = "PAY_PER_REQUEST"  # Pay only for what you use
   hash_key       = "LockID"
   
-  point_in_time_recovery {
-    enabled = true
-  }
-  
-  server_side_encryption {
-    enabled     = true
-    kms_key_arn = local.kms_key_exists ? data.aws_kms_alias.terraform_key.target_key_id : aws_kms_key.terraform_key[0].arn
-  }
-  
   attribute {
     name = "LockID"
     type = "S"  # String type attribute
